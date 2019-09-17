00590cdc94a7ad5518122b3c978f14404c0bd83b
Bill Denney <wdenney@humanpredictions.com>
2019-09-17 12:29:27 -0400

    Combine and simplify documentation for calc_derived_*() functions

--- R/calc_derived_1cpt.R
+++ R/calc_derived_1cpt.R
@@ -1,56 +1,37 @@
-#' Calculate derived pharmacokinetic parameters for a 1-compartment linear model
-#'
-#' @param CL Clearance (L/h)
-#' @param V  Central volume of distribution (L)
-#' @param type Type of half-life to return (\code{"Vss"}, \code{"k10"}, \code{"thalf"}, \code{"alpha"}, \code{"trueA"}, \code{"fracA"}, \code{"all"}). Default is \code{"all"}).
-#' @param sigdig Number of significant digits to be returned. Default is 5.
-#' 
-#' @return Return a list of derived PK parameters for a 1-compartment linear model given provided clearances and volumes.
-#' \itemize{ 
-#'   \item \code{Vss}: \eqn{V_{ss}} (L)
-#'   \item \code{k10}: \eqn{k_{10}} (/h)
-#'   \item \code{thalf}: \eqn{t_{1/2}} (h)
-#'   \item \code{alpha}: \eqn{\alpha}
-#'   \item \code{trueA}: true A
-#'   \item \code{fracA}: fractional A
-#'  }
-#'
-#' @author Justin Wilkins, \email{justin.wilkins@@occams.com}
-#' @references Shafer S. L. \code{CONVERT.XLS}
-#' @references Rowland M, Tozer TN. Clinical Pharmacokinetics and Pharmacodynamics: Concepts and Applications (4th). Lippincott Williams & Wilkins, Philadelphia, 2010.  
-#'
+#' @rdname calc_derived_3cpt
 #' @examples
 #' params <- calc_derived_1cpt(CL=16, V=25)
-#'
 #' @export
 
-calc_derived_1cpt <- function(CL, V, type="all", sigdig=5) {
+calc_derived_1cpt <- function(CL, V=NULL, V1=NULL, type="all", sigdig=5) {
+  if (!xor(is.null(V), is.null(V1))) {
+    stop("Exactly one of V or V1 may be provided since they are considered synonyms.")
+  } else if (!is.null(V)) {
+    # Use the preferred synonym
+    V1 <- V
+  }
+  k10 <- CL/V1
+  Vss <- V1
+  trueA <- 1/V1
+  thalf <- log(2)/k10
+  alpha <- k10
+  out <-
+    list(
+      k10=signif(CL/V1, sigdig),
+      Vss=signif(Vss, sigdig),
+      thalf=signif(thalf, sigdig),
+      alpha=signif(alpha, sigdig),
+      trueA=signif(1/V1, sigdig),
+      fracA=1
+    )
 
-  out <- list()
-  
-  ### variables
-  out$k10   <- signif(CL/V, sigdig)
-
-  out$Vss <- V
-  
-  out$thalf <- signif(log(2)/out$k10, sigdig)
-  
-  out$alpha <- out$k10
-  
-  out$trueA <- signif(1/V, sigdig)
-  out$fracA <- 1
-  
-  
-  if(type=="all") o <- out
-  if(type=="k10") o <- out$k10
-  if(type=="trueA") o <- out$trueA
-  if(type=="fracA") o <- out$fracA
-  if(type=="thalf") o <- out$thalf
-  if(type=="alpha") o <- out$alpha
-  if(type=="Vss") o <- out$Vss
-  
+  if(type=="all") {
+    o <- out
+  } else {
+    o <- out[[type]]
+    if (is.null(o)) {
+      stop("Invalid value for `type`: ", type)
+    }
+  }
   o
-
-  
 }
-
--- R/calc_derived_3cpt.R
+++ R/calc_derived_3cpt.R
@@ -1,34 +1,34 @@
-#' Calculate derived pharmacokinetic parameters for a 3-compartment linear model
+#' Calculate derived pharmacokinetic parameters for a 1-, 2-, or 3-compartment linear model.
 #'
 #' @param CL Clearance (L/h)
-#' @param V1 Central volume of distribution (L)
+#' @param V1,V Central volume of distribution (L).  Values are synonyms; use only one.
 #' @param V2 First peripheral volume of distribution (L)
 #' @param V3 Second peripheral volume of distribution (L)
-#' @param Q2 Intercompartmental clearance from central to first peripheral compartment (L/h)
+#' @param Q2,Q Intercompartmental clearance from central to first peripheral compartment (L/h).  Values are synonyms; use only one.
 #' @param Q3 Intercompartmental clearance from central to second peripheral compartment (L/h)
-#' @param type Type of half-life to return (\code{"Vss"}, \code{"k10"}, \code{"k12"}, \code{"k13"}, \code{"k21"}, \code{"k31"}, \code{"thalf_alpha"}, \code{"thalf_beta"}, \code{"thalf_gamma"}, \code{"alpha"}, \code{"beta"}, \code{"gamma"}, \code{"trueA"}, \code{"trueB"}, \code{"trueC"}, \code{"fracA"}, \code{"fracB"}, \code{"fracC"}, \code{"all"}). Default is \code{"all"}).
-#' @param sigdig Number of significant digits to be returned. Default is 5.
+#' @param type Type of half-life to return. Default is \code{"all"}; see details for other options that are specific to the number of compartments.
+#' @param sigdig Number of significant digits to be returned. Default is \code{5}.
 #' 
-#' @return Return a list of derived PK parameters for a 3-compartment linear model given provided clearances and volumes.
+#' @return Return a list of derived PK parameters for a 3-compartment linear model given provided clearances and volumes based on the `type`.
 #' \itemize{ 
-#'   \item \code{Vss}: Volume of distribution at steady state, \eqn{V_{ss}} (L)
-#'   \item \code{k10}: First-order elimination rate, \eqn{k_{10}} (/h)
-#'   \item \code{k12}: First-order rate of transfer from central to first peripheral compartment, \eqn{k_{12}} (/h)
-#'   \item \code{k21}: First-order rate of transfer from first peripheral to central compartment, \eqn{k_{21}} (/h)   
-#'   \item \code{k13}: First-order rate of transfer from central to second peripheral compartment, \eqn{k_{13}} (/h)
-#'   \item \code{k31}: First-order rate of transfer from second peripheral to central compartment,\eqn{k_{31}} (/h)  
-#'   \item \code{thalf_alpha}: \eqn{t_{1/2,\alpha}} (h)
-#'   \item \code{thalf_beta}: \eqn{t_{1/2,\beta}} (h)
-#'   \item \code{thalf_gamma}: \eqn{t_{1/2,\gamma}} (h)
-#'   \item \code{alpha}: \eqn{\alpha}
-#'   \item \code{beta}: \eqn{\beta}
-#'   \item \code{gamma}: \eqn{\beta}
-#'   \item \code{trueA}: true A
-#'   \item \code{trueB}: true B
-#'   \item \code{trueC}: true C
-#'   \item \code{fracA}: fractional A
-#'   \item \code{fracB}: fractional B
-#'   \item \code{fracC}: fractional C
+#'   \item \code{Vss}: Volume of distribution at steady state, \eqn{V_{ss}} (L); 1-, 2-, and 3-compartment
+#'   \item \code{k10}: First-order elimination rate, \eqn{k_{10}} (/h); 1-, 2-, and 3-compartment
+#'   \item \code{k12}: First-order rate of transfer from central to first peripheral compartment, \eqn{k_{12}} (/h); 2- and 3-compartment
+#'   \item \code{k21}: First-order rate of transfer from first peripheral to central compartment, \eqn{k_{21}} (/h); 2- and 3-compartment
+#'   \item \code{k13}: First-order rate of transfer from central to second peripheral compartment, \eqn{k_{13}} (/h); 3-compartment
+#'   \item \code{k31}: First-order rate of transfer from second peripheral to central compartment,\eqn{k_{31}} (/h); 3-compartment
+#'   \item \code{thalf_alpha}: \eqn{t_{1/2,\alpha}} (h); 2- and 3-compartment
+#'   \item \code{thalf_beta}: \eqn{t_{1/2,\beta}} (h); 2- and 3-compartment
+#'   \item \code{thalf_gamma}: \eqn{t_{1/2,\gamma}} (h); 3-compartment
+#'   \item \code{alpha}: \eqn{\alpha}; 1-, 2-, and 3-compartment
+#'   \item \code{beta}: \eqn{\beta}; 2- and 3-compartment
+#'   \item \code{gamma}: \eqn{\beta}; 3-compartment
+#'   \item \code{trueA}: true A; 1-, 2-, and 3-compartment
+#'   \item \code{trueB}: true B; 2- and 3-compartment
+#'   \item \code{trueC}: true C; 3-compartment
+#'   \item \code{fracA}: fractional A; 1-, 2-, and 3-compartment
+#'   \item \code{fracB}: fractional B; 2- and 3-compartment
+#'   \item \code{fracC}: fractional C; 3-compartment
 #'  }
 #'
 #' @author Justin Wilkins, \email{justin.wilkins@@occams.com}
@@ -40,10 +40,20 @@
 #'
 #' @export
 
-calc_derived_3cpt <- function(CL, V1, V2, V3, Q2, Q3, type="all", sigdig=5) {
+calc_derived_3cpt <- function(CL, V1, V2, V3, Q2, Q3, V=NULL, Q=NULL, type="all", sigdig=5) {
+  if (!xor(is.null(V), is.null(V1))) {
+    stop("Exactly one of V or V1 may be provided since they are considered synonyms.")
+  } else if (!is.null(V)) {
+    # Use the preferred synonym
+    V1 <- V
+  }
+  if (!xor(is.null(Q), is.null(Q2))) {
+    stop("Exactly one of Q or Q2 may be provided since they are considered synonyms.")
+  } else if (!is.null(Q)) {
+    # Use the preferred synonym
+    Q2 <- Q
+  }
 
-  out <- list()
-  
   ### variables
   k10   <- CL/V1
   k12   <- Q2/V1
@@ -73,51 +83,39 @@ calc_derived_3cpt <- function(CL, V1, V2, V3, Q2, Q3, type="all", sigdig=5) {
   c2 <- (k21 - i2) * (k31 - i2) / (i2 - i1) / (i2 - i3) / V1
   c3 <- (k21 - i3) * (k31 - i3) / (i3 - i2) / (i3 - i1) / V1
 
-  out$k10 <- signif(k10, sigdig)
-  out$k12 <- signif(k12, sigdig)
-  out$k21 <- signif(k21, sigdig)
-  out$k13 <- signif(k13, sigdig)
-  out$k31 <- signif(k31, sigdig)
-  
-  out$Vss <- signif(V1 + V2 + V3, sigdig)
-  
-  out$thalf_alpha <- signif(log(2)/i1, sigdig)
-  out$thalf_beta  <- signif(log(2)/i2, sigdig)
-  out$thalf_gamma <- signif(log(2)/i3, sigdig)
-  
-  out$alpha <- signif(i1, sigdig)
-  out$beta  <- signif(i2, sigdig)
-  out$gamma <- signif(i3, sigdig)
-  
-  out$trueA <- signif(c1, sigdig)
-  out$trueB <- signif(c2, sigdig)
-  out$trueC <- signif(c3, sigdig)
-  
-  out$fracA <- signif(c1*V1, sigdig)
-  out$fracB <- signif(c2*V1, sigdig)
-  out$fracC <- signif(c3*V1, sigdig)
-  
-  if(type=="all") o <- out
-  if(type=="k10") o <- out$k10
-  if(type=="k12") o <- out$k12
-  if(type=="k21") o <- out$k21
-  if(type=="k13") o <- out$k13
-  if(type=="k31") o <- out$k31
-  if(type=="trueA") o <- out$trueA
-  if(type=="trueB") o <- out$trueB
-  if(type=="trueC") o <- out$trueC
-  if(type=="fracA") o <- out$fracA
-  if(type=="fracB") o <- out$fracB
-  if(type=="fracC") o <- out$fracC
-  if(type=="thalf_alpha") o <- out$thalf_alpha
-  if(type=="thalf_beta") o <- out$thalf_beta
-  if(type=="thalf_gamma") o <- out$thalf_gamma
-  if(type=="alpha") o <- out$alpha
-  if(type=="beta") o <- out$beta
-  if(type=="gamma") o <- out$gamma
-  if(type=="Vss") o <- out$Vss
-  
+  out <-
+    list(
+      k10=signif(k10, sigdig),
+      k12=signif(k12, sigdig),
+      k21=signif(k21, sigdig),
+      k13=signif(k13, sigdig),
+      k31=signif(k31, sigdig),
+      
+      Vss=signif(V1 + V2 + V3, sigdig),
+      
+      thalf_alpha=signif(log(2)/i1, sigdig),
+      thalf_beta =signif(log(2)/i2, sigdig),
+      thalf_gamma=signif(log(2)/i3, sigdig),
+      
+      alpha=signif(i1, sigdig),
+      beta =signif(i2, sigdig),
+      gamma=signif(i3, sigdig),
+      
+      trueA=signif(c1, sigdig),
+      trueB=signif(c2, sigdig),
+      trueC=signif(c3, sigdig),
+      
+      fracA=signif(c1*V1, sigdig),
+      fracB=signif(c2*V1, sigdig),
+      fracC=signif(c3*V1, sigdig)
+    )
+  if(type=="all") {
+    o <- out
+  } else {
+    o <- out[[type]]
+    if (is.null(o)) {
+      stop("Invalid value for `type`: ", type)
+    }
+  }
   o
-  
 }
-
