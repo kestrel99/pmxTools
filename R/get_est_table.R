e0caa8c91c20e8b749b6243ae5f831049f92a753
Justin Wilkins <justin.wilkins@occams.com>
2019-10-28 13:45:10 +0100

    Fixes to get_shrinkage and get_est_table

--- R/get_est_table.R
+++ R/get_est_table.R
@@ -19,103 +19,86 @@
 #'
 #' @export
 
-get_est_table <- function(x, thetaLabels=c(), omegaLabels=c(),
-                        sigmaLabels=c(), sigdig=3) {
-
-  theta     <- get_theta(x, sigdig=3)
-  thetarse  <- get_theta(x, output="rse", sigdig=3)
-  theta95ci <- get_theta(x, output="95ci", sigdig=3)
-
-  omegaMatrix     <- get_omega(x, sigdig=3)
-  omegaRSEMatrix  <- get_omega(x, sigdig=3, output="rse")
-  omega95CIMatrix <- get_omega(x, sigdig=3, output="95ci")
-
-  omegaL     <- c()
-  omegaRSEL  <- c()
+get_est_table <- function (x, thetaLabels = c(), omegaLabels = c(),
+                           sigmaLabels = c(), sigdig = 3) {
+  theta <- get_theta(x, sigdig = 3)
+  thetarse <- get_theta(x, output = "rse", sigdig = 3)
+  theta95ci <- get_theta(x, output = "95ci", sigdig = 3)
+  omegaMatrix <- get_omega(x, sigdig = 3)
+  omegaRSEMatrix <- get_omega(x, sigdig = 3, output = "rse")
+  omega95CIMatrix <- get_omega(x, sigdig = 3, output = "95ci")
+  
+  omegaL <- c()
+  omegaRSEL <- c()
   omega95CIL <- c()
-  omegaLab   <- c()
-  for(n in 1:nrow(omegaMatrix)) {
-    omegaL     <- c(omegaL, omegaMatrix[n,1:n])
-    omegaRSEL  <- c(omegaRSEL, omegaRSEMatrix[n,1:n])
-    omega95CIL <- c(omega95CIL, omega95CIMatrix[n,1:n])
-    omegaLab   <- c(omegaLab, paste("OM", n, ",", 1:n, sep=""))
+  omegaLab <- c()
+  
+  for (n in 1:nrow(omegaMatrix)) {
+    omegaL <- c(omegaL, omegaMatrix[n, 1:n])
+    omegaRSEL <- c(omegaRSEL, omegaRSEMatrix[n, 1:n])
+    omega95CIL <- c(omega95CIL, omega95CIMatrix[n, 1:n])
+    omegaLab <- c(omegaLab, paste("OM", n, ",", 1:n, sep = ""))
   }
-
-  sigmaMatrix     <- get_sigma(x, sigdig=3)
-  sigmaRSEMatrix  <- get_sigma(x, sigdig=3, output="rse")
-  sigma95CIMatrix <- get_sigma(x, sigdig=3, output="95ci")
-
-  sigmaL     <- c()
-  sigmaRSEL  <- c()
+  sigmaMatrix <- get_sigma(x, sigdig = 3)
+  sigmaRSEMatrix <- get_sigma(x, sigdig = 3, output = "rse")
+  sigma95CIMatrix <- get_sigma(x, sigdig = 3, output = "95ci")
+  sigmaL <- c()
+  sigmaRSEL <- c()
   sigma95CIL <- c()
-  sigmaLab   <- c()
-  for(n in 1:nrow(sigmaMatrix)) {
-    sigmaL     <- c(sigmaL, sigmaMatrix[n,1:n])
-    sigmaRSEL  <- c(sigmaRSEL, sigmaRSEMatrix[n,1:n])
-    sigma95CIL <- c(sigma95CIL, sigma95CIMatrix[n,1:n])
-    sigmaLab   <- c(sigmaLab, paste("SI", n, ",", 1:n, sep=""))
+  sigmaLab <- c()
+  for (n in 1:nrow(sigmaMatrix)) {
+    sigmaL <- c(sigmaL, sigmaMatrix[n, 1:n])
+    sigmaRSEL <- c(sigmaRSEL, sigmaRSEMatrix[n, 1:n])
+    sigma95CIL <- c(sigma95CIL, sigma95CIMatrix[n, 1:n])
+    sigmaLab <- c(sigmaLab, paste("SI", n, ",", 1:n, sep = ""))
   }
-
-  labCol <- paste("THETA", 1:length(theta),sep="")
-
-  if(length(thetaLabels)==length(theta)) {
+  labCol <- paste("THETA", 1:length(theta), sep = "")
+  if (length(thetaLabels) == length(theta)) {
     labCol <- thetaLabels
   }
-
   idxOmegaDiag <- c()
-  for (n in 1:nrow(omegaMatrix)){
+  for (n in 1:nrow(omegaMatrix)) {
     idxOmegaDiag <- c(idxOmegaDiag, sum(1:n))
   }
   idxOmegaOffDiag <- setdiff(1:length(omegaL), idxOmegaDiag)
-
-  if(length(omegaLabels)==nrow(omegaMatrix)) {
+  if (length(omegaLabels) == nrow(omegaMatrix)) {
     omegaLab[idxOmegaDiag] <- omegaLabels
   } else {
-    omegaLab[idxOmegaDiag] <- paste("OMEGA", 1:nrow(omegaMatrix), sep="")
+    omegaLab[idxOmegaDiag] <- paste("OMEGA", 1:nrow(omegaMatrix), 
+                                    sep = "")
   }
-
   labCol <- c(labCol, omegaLab)
-
   idxSigmaDiag <- c()
-  for (n in 1:nrow(sigmaMatrix)){
+  for (n in 1:nrow(sigmaMatrix)) {
     idxSigmaDiag <- c(idxSigmaDiag, sum(1:n))
   }
   idxSigmaOffDiag <- setdiff(1:length(sigmaL), idxSigmaDiag)
-
-  if(length(sigmaLabels)==nrow(sigmaMatrix)) {
+  if (length(sigmaLabels) == nrow(sigmaMatrix)) {
     sigmaLab[idxSigmaDiag] <- sigmaLabels
   } else {
-    sigmaLab[idxSigmaDiag] <- paste("SIGMA", 1:nrow(sigmaMatrix), sep="")
+    sigmaLab[idxSigmaDiag] <- paste("SIGMA", 1:nrow(sigmaMatrix), 
+                                    sep = "")
   }
-
   labCol <- c(labCol, sigmaLab)
-
-  estCol <- c(as.numeric(theta),
-              as.numeric(omegaL),
-              as.numeric(sigmaL))
-
-  rseCol <- c(as.numeric(thetarse),
-              as.numeric(omegaRSEL),
+  estCol <- c(as.numeric(theta), as.numeric(omegaL), as.numeric(sigmaL))
+  rseCol <- c(as.numeric(thetarse), as.numeric(omegaRSEL), 
               as.numeric(sigmaRSEL))
-
-  CI95Col <- c(as.character(theta95ci),
-               as.character(omega95CIL),
+  CI95Col <- c(as.character(theta95ci), as.character(omega95CIL), 
                as.character(sigma95CIL))
-
-  shrEta <- rep("-", times=length(omegaL))
+  shrEta <- rep("-", times = length(omegaL))
   shrEta[idxOmegaDiag] <- as.numeric(get_shrinkage(x))
-
-  shrCol <- c(rep("-", times=length(theta)),
-              shrEta,
-              as.numeric(get_shrinkage(x, output="epsilon")))
-
-  out <- data.frame(Parameter = labCol,
-                    Estimate  = estCol,
-                    RSE       = rseCol,
-                    CI95      = CI95Col,
-                    Shrinkage = shrCol)
-
-
-
+  
+  shrEps <- as.numeric(get_shrinkage(x, 
+                                     output = "epsilon"))
+  if(length(sigmaL>1)) {
+    shrEps <- rep("-", times = length(sigmaL))
+    shrEps[idxSigmaDiag] <- as.numeric(get_shrinkage(x, output="epsilon"))      
+  }
+  
+  shrCol <- c(rep("-", times = length(theta)), shrEta, shrEps)
+  out <- data.frame(Parameter = labCol, Estimate = estCol, 
+                    RSE = rseCol, CI95 = CI95Col, Shrinkage = shrCol)
   out
+  
 }
+
--- R/get_shrinkage.R
+++ R/get_shrinkage.R
@@ -3,6 +3,8 @@
 #' @param x A NONMEM output object generated using \code{\link{read_nm}}.
 #' @param output A flag specifying the shrinkage estimates to be output. Valid flag
 #' values are \code{eta} (the default), \code{epsilon}, or \code{all}.
+#' @param type Specifies the type of shrinkage to report. Valid values are \code{sd} 
+#' (standard deviation, the default) or \code{vr} (variance, if present in the XML output). 
 #' @param sigdig Specifies the number of significant digits to be provided (default=3).
 #'
 #' @return A named vector of NONMEM shrinkage estimates, or in the case of \code{all},
@@ -23,40 +25,73 @@
 #'
 #' @export
 
-get_shrinkage <- function(x, output = "eta", sigdig = 3) {
+get_shrinkage <- function (x, output = "eta", type="sd", sigdig = 3) {
   if (!(output %in% c("eta", "epsilon", "all"))) {
     stop("Please select a valid output option (eta, epsilon, all).")
   }
-
-  eta <-
-    signif(as.numeric(x$nonmem$problem$estimation$etashrink[seq(1,
-                                                                length(x$nonmem$problem$estimation$etashrink) -
-                                                                  1, by = 2)]),
-           sigdig)
-  names(eta) <-
-    as.character(x$nonmem$problem$estimation$etashrink[seq(2,
-                                                           length(x$nonmem$problem$estimation$etashrink) -
-                                                             1, by = 2)])
-
-  eps <-
-    signif(as.numeric(x$nonmem$problem$estimation$epsshrink[seq(1,
-                                                                length(x$nonmem$problem$estimation$epsshrink) -
-                                                                  1, by = 2)]),
-           sigdig)
-  names(eps) <-
-    as.character(x$nonmem$problem$estimation$epsshrink[seq(2,
-                                                           length(x$nonmem$problem$estimation$epsshrink) -
-                                                             1, by = 2)])
-
-  if (output == "eta")
-    out <- eta
-  if (output == "epsilon")
-    out <- eps
-  if (output == "all") {
-    out <- list()
-    out$eta <- eta
-    out$epsilon <- eps
+  if (!(type %in% c("sd", "vr"))) {
+    stop("Please select a valid shrinkage type option (sd, vr).")
   }
-
+  
+  if(!is.null(x$nonmem$problem$estimation$etashrink)) {
+    eta <- signif(as.numeric(x$nonmem$problem$estimation$etashrink[seq(1, 
+                                                                       length(x$nonmem$problem$estimation$etashrink) - 1, by = 2)]), 
+                  sigdig)
+    names(eta) <- as.character(x$nonmem$problem$estimation$etashrink[seq(2, 
+                                                                         length(x$nonmem$problem$estimation$etashrink) - 1, by = 2)])
+    eps <- signif(as.numeric(x$nonmem$problem$estimation$epsshrink[seq(1, 
+                                                                       length(x$nonmem$problem$estimation$epsshrink) - 1, by = 2)]), 
+                  sigdig)
+    names(eps) <- as.character(x$nonmem$problem$estimation$epsshrink[seq(2, 
+                                                                         length(x$nonmem$problem$estimation$epsshrink) - 1, by = 2)])
+  } else {
+    eta <- signif(as.numeric(x$nonmem$problem$estimation$etashrinksd[seq(1, 
+                                                                         length(x$nonmem$problem$estimation$etashrinksd) - 1, by = 2)]), 
+                  sigdig)
+    names(eta) <- as.character(x$nonmem$problem$estimation$etashrinksd[seq(2, 
+                                                                           length(x$nonmem$problem$estimation$etashrinksd) - 1, by = 2)])
+    eps <- signif(as.numeric(x$nonmem$problem$estimation$epsshrinksd[seq(1, 
+                                                                         length(x$nonmem$problem$estimation$epsshrinksd) - 1, by = 2)]), 
+                  sigdig)
+    names(eps) <- as.character(x$nonmem$problem$estimation$epsshrinksd[seq(2, 
+                                                                           length(x$nonmem$problem$estimation$epsshrinksd) - 1, by = 2)]) 
+    etavr <- signif(as.numeric(x$nonmem$problem$estimation$etashrinkvr[seq(1, 
+                                                                           length(x$nonmem$problem$estimation$etashrinkvr) - 1, by = 2)]), 
+                    sigdig)
+    names(etavr) <- as.character(x$nonmem$problem$estimation$etashrinkvr[seq(2, 
+                                                                             length(x$nonmem$problem$estimation$etashrinkvr) - 1, by = 2)])
+    epsvr <- signif(as.numeric(x$nonmem$problem$estimation$epsshrinkvr[seq(1, 
+                                                                           length(x$nonmem$problem$estimation$epsshrinkvr) - 1, by = 2)]), 
+                    sigdig)
+    names(epsvr) <- as.character(x$nonmem$problem$estimation$epsshrinkvr[seq(2, 
+                                                                             length(x$nonmem$problem$estimation$epsshrinkvr) - 1, by = 2)]) 
+  }
+  if (output == "eta" & type=="sd") {
+    out <- eta
+  }
+  if (output == "eta" & type=="vr") {
+    out <- etavr
+  }
+  
+  if (output == "epsilon" & type=="sd") {
+    out <- eps
+  }
+  if (output == "epsilon" & type=="vr") {
+    out <- epsvr
+  }  
+  
+  if (output == "all") {
+    if(!is.null(x$nonmem$problem$estimation$etashrink)) {
+      out <- list()
+      out$etasd <- eta
+      out$etavr <- etavr
+      out$epsilonsd <- eps
+      out$epsilonvr <- epsvr
+    } else {
+      out <- list()
+      out$etasd <- eta
+      out$epsilonsd <- eps
+    }
+  } 
   out
 }
